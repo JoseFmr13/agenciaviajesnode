4a59f7165a6455ded08a18dc876461870940bb4f
Jose Medina <jm02130213@gmail.com.com>
2026-05-28 03:08:39 -0700

    Fix: puerto de conexion mysql en db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,32 @@
+// import Sequelize from 'sequelize';
+// import dotenv from 'dotenv';
+// dotenv.config()
+
+
+// const db = new Sequelize( process.env.DATABASE_URL, {
+//     define: {
+//         timestamps: false
+//     },
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// });
+
+// export default db;
+
+
+
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
-
-const db = new Sequelize( process.env.DATABASE_URL, {
+const db = new Sequelize(process.env.DATABASE_URL, {
+    host: 'wsjdhs.h.filess.io', // Obliga a Sequelize a apuntar aquí
+    port: 3307,                 // Obliga a usar el puerto correcto
+    dialect: 'mysql',           // Define explícitamente el dialecto
     define: {
         timestamps: false
     },
